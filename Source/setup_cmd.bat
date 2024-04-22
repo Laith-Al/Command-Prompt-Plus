0b54cc7b709f90e59de7ed3e7df85f51a4cc39c1
Laith Alshara <laithalshara10@gmail.com>
2024-04-22 11:56:29 +1000

    CMD

---
+++ Source/setup_cmd.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+Title Installation Of Command Prompt Plus!
+cls
+echo.
+echo Installatoin Of Command Prompt Plus:
+echo.
+echo Thank you for installing CMD Plus!
+echo Please Agree To The Following:
+echo.
+echo 1. Make sure you are installing the **Real** Command Prompt Plus From The Offical Github Page!
+echo.
+echo 2. Make sure you run through everything clearly, Un-installing Command Prompt Plus Is Not An Easy Task.
