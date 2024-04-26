5c96b9ab2e8ac68e0cf3a677cd75d20cbf44b1fd
Laith Alshara <laithalshara10@gmail.com>
2024-04-26 11:32:03 +1000

    Huge Update

---
+++ Source/bios.bat
@@ -0,0 +1,33 @@
+@echo off
+cls
+color 4
+echo.
+echo ---------------------------------------------
+echo Are You Sure You Want To Restart To The Bios?
+echo ---------------------------------------------
+echo.
+echo Would you like to continue? (Y/N): 
+set /p choice=
+
+if /i "%choice%"=="Y" (
+    goto Yes
+) else if /i "%choice%"=="N" (
+    goto No
+) else (
+    echo Invalid choice. Please try again.
+    pause
+    goto menu
+)
+
+:Yes
+echo You chose to continue.
+shutdown /r /fw /f /t 0
+exit
+
+:No
+echo You chose to stop.
+exit
+goto End
+
+:End
+exit
