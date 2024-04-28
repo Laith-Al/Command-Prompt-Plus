99c45d92b0c3550640557a70aa1e13ef10633afb
Laith Alshara <laithalshara10@gmail.com>
2024-04-28 12:30:27 +1000

    Majour Source Update!

--- Source/assist.bat
+++ Source/assist.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBdVRRCKAES0A5EO4f7+086IoVgQUewra7P806CmNeIvyUrpeJcj0jdqisQBHA8VTh6+fTMZqH1Ws3a6AcKOoAbuRHSI9Vg5CHE6gnvV7A==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 echo.
 echo Command Prompt Plus Commands:
@@ -52,13 +19,21 @@ echo sessionexpl: Session Exploiter (Login To Other User's Account Without Passw
 echo.
 echo IP: Shows Public IP Address
 echo.
-echo User: Who is Signed In?
+echo user: Who is Signed In?
 echo.
-echo Credit: Shows Credit For This Software
+echo credit: Shows Credit For This Software
 echo.
-echo Pin: Generate a secure 4 digit pin
+echo pin: Generate a secure 4 digit pin
 echo.
 echo psg: Generate a password. Type in either PSG8 or PSG12 to generate a 8 character password or 12 character password
 echo.
-echo 
+echo bios: Reboot Directly To Your BIOS
+echo.
+echo matrix: Shows you a green Matrix. Type in matrixRGB to get an RGB Matrix
+echo.
+echo tasks: View all open tasks
+echo.
+echo user: Tells you who is logged in currently.
+echo.
+echo restart: Restart your computer!
 @echo on
---
+++ Source/restart.bat
@@ -0,0 +1,6 @@
+@echo off
+echo.
+echo Restarting Computer
+echo.
+shutdown /r 
+@echo on
